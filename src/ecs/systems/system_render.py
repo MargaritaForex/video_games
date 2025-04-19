ebe99e02d1a507067fe238ab7d62790d3ef4f14b
milindr4123 <e.foreros@uniandes.edu.co>
2025-04-19 14:27:52 -0500

    fix class to implement ECS

--- src/ecs/systems/system_render.py
+++ src/ecs/systems/system_render.py
@@ -1,12 +1,14 @@
 # src/ecs/systems/system_render.py
 import pygame
-from src.ecs.components.CPosition import CPosition
+from src.ecs.components.CTransform import CTransform
 from src.ecs.components.CSurface import CSurface
+
 
 def system_render(world, screen):
     for entity_id, components in world.items():
-        pos = components.get(CPosition)
+        transform = components.get(CTransform)
         surface = components.get(CSurface)
-        
-        if pos and surface:
-            screen.blit(surface.surface, (pos.x, pos.y))
+
+        if transform and surface:
+            screen.blit(surface.surface, (transform.pos.x, transform.pos.y))
+
