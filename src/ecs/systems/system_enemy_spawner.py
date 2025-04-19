95cc9f178958600f9fb81e9dca28099ecdfb4d6a
milindr4123 <e.foreros@uniandes.edu.co>
2025-04-19 13:12:03 -0500

    fix class to implement ECS

--- src/ecs/systems/system_enemy_spawner.py
+++ src/ecs/systems/system_enemy_spawner.py
@@ -2,8 +2,11 @@
 from src.ecs.components.CEnemySpawner import CEnemySpawner
 from src.create.prefab_creator import PrefabCreator
 
+
 def system_enemy_spawner(engine, prefab_creator: PrefabCreator, delta_time):
-    for entity_id, components in engine.entities.items():
+    # Clonamos la lista de llaves (IDs de entidades)
+    for entity_id in list(engine.entities.keys()):
+        components = engine.entities[entity_id]
         spawner = components.get(CEnemySpawner)
         if spawner:
             spawner.timer += delta_time
@@ -11,11 +14,11 @@ def system_enemy_spawner(engine, prefab_creator: PrefabCreator, delta_time):
                 if idx not in spawner.spawned and spawner.timer >= spawn_event["time"]:
                     enemy_type = spawn_event["enemy_type"]
                     pos = spawn_event["position"]
-                    
-                    # Crear el enemigo
+
                     if enemy_type == "Hunter":
                         prefab_creator.create_hunter(pos)
                     else:
                         prefab_creator.create_enemy(pos, enemy_type)
-                    
+
                     spawner.spawned.add(idx)
+
